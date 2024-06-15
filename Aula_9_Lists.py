97da0b759083c970e73975327b1d915a9efb70e7
Claive Monteza <43734209+claivemonteza@users.noreply.github.com>
2024-06-15 21:06:03 +0200

    Update Aula_9_Lists.py

--- Aula_9_Lists.py
+++ Aula_9_Lists.py
@@ -5,6 +5,9 @@ names = ['Claive','Rolando', 'De','Sousa','Monteza']
 # print all list
 print(names)
 
+# print all the item on the list from the beging to the end
+print(names[:])
+
 # First name on the list
 print(names[0])
 
@@ -17,6 +20,9 @@ print(names)
 
 # Range values
 print(names[0:4])
+
+# Will print all the item start with the index 2 to the end the list
+print(names[2:])
 
 
 #LIST METHODS
